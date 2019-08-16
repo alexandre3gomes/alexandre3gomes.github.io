9025b6b1ea6a28f567310907541696a7d5a209a1
Alexandre Gomes da Silva <alexandre.gomes.silva@celfocus.com>
2019-08-16 15:02:39 +0100

    Implemented simple resume

--- root.js
+++ root.js
@@ -1,6 +1,82 @@
 const root = new Vue({
     el: '#root',
     data: {
-        skills: ['Java', 'MySql', 'Vue']
+        skills: [{
+                name: 'Java',
+                level: 5
+            },
+            {
+                name: 'SQL',
+                level: 4
+            },
+            {
+                name: 'JSF',
+                level: 4
+            },
+            {
+                name: 'Spring',
+                level: 4
+            },
+            {
+                name: 'JPA',
+                level: 3
+            },
+            {
+                name: 'Hibernate',
+                level: 3
+            },
+            {
+                name: 'Git',
+                level: 4
+            },
+            {
+                name: 'Maven',
+                level: 3
+            },
+            {
+                name: 'Scrum',
+                level: 5
+            },
+            {
+                name: 'Html 5',
+                level: 3
+            },
+            {
+                name: 'CSS3',
+                level: 3
+            },
+            {
+                name: 'Node',
+                level: 2
+            },
+            {
+                name: 'React',
+                level: 2
+            },
+            {
+                name: 'Angular 2+',
+                level: 4
+            },
+            {
+                name: 'UML',
+                level: 4
+            },
+            {
+                name: 'CI',
+                level: 3
+            },
+            {
+                name: 'JUnit',
+                level: 3
+            },
+            {
+                name: 'Mock',
+                level: 3
+            },
+            {
+                name: 'Selenium',
+                level: 3
+            }
+        ]
     }
 });
